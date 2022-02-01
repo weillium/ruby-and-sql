760734e0106b1da23754385488117923f9f3258f
weillium <will.liao@kellogg.northwestern.edu>
2022-02-01 17:21:38 +0000

    code-along migrate finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_180405) do
+ActiveRecord::Schema.define(version: 2022_02_01_171537) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2022_01_31_180405) do
     t.string "email"
     t.string "phone_number"
     t.integer "company_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "salespeople", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
